42cf857bfc209baef7d99c9efb878a171e9674cb
Joshua E. Roybal <oysbharljo@yahoo.com>
2017-09-26 13:43:15 -0500

    Delete make.sh

--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -v
-#g++ -O2 -c qclass.cpp
-g++ -O2 -c qdriver.cpp
-g++ -s -o nqueens.cgi qdriver.o qclass.o
-sudo cp nqueens.cgi /home/http/cgi-bin/nqueens.cgi
-sudo chown apache:apache /home/http/cgi-bin/nqueens.cgi
